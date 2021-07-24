d05f88b666551d3670e3d9398762477ed166a875
Rudraksh27 <85703923+Rudraksh27@users.noreply.github.com>
2021-07-24 12:27:55 +0530

    src

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about content
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, qui voluptate. Dolorum reiciendis asperiores numquam quidem ullam nostrum rerum sapiente molestiae officia, optio porro iste laboriosam esse, possimus obcaecati pariatur!
+                Sequi, molestiae rerum, deleniti ab odit quidem dolores voluptates commodi adipisci beatae velit officia vel! Assumenda sunt illum rem sapiente distinctio, aperiam enim voluptate quaerat adipisci cupiditate. Sint, possimus repellendus.
+                Eos adipisci aliquam alias repellat ipsam pariatur dolorem, molestias libero impedit. Perspiciatis, possimus optio consequuntur suscipit ab vero. Ullam delectus odit reiciendis rem debitis officiis aperiam quo, sint dolore modi.
+                Quidem suscipit labore dicta aperiam et soluta laudantium repudiandae amet voluptate ea quasi tenetur, rerum iure dolore voluptas? Minima itaque facere natus provident incidunt obcaecati quibusdam voluptatum esse. Doloribus, nisi.
+                Sunt repellendus facere a. Dolorem qui commodi veritatis sunt necessitatibus saepe omnis possimus in assumenda velit sequi expedita id voluptas, reiciendis sapiente numquam, ab nobis cupiditate? Aliquid quidem rem ipsam.</p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+/* import './Footer.css' */
+
+export const Footer = () => {
+    /* let footerStyle = {
+
+
+    } */
+    return (
+        <footer className="text-light py-3 rudra" style={{
+            backgroundColor: "red",
+            position: "relative",
+            top: "70vh",
+            width: "100%"
+        }
+
+        } >
+            <p className="text-center">
+                Copyright &copy; Mytodoslist.com
+            </p>
+        </footer>
+    )
+}
